f38f796ec51cad985d3ac516e3f09e97c55474b8
phuclevinh2000 <47014132+phuclevinh2000@users.noreply.github.com>
2021-06-02 14:05:51 +0300

    first commit

---
+++ Ex_Files_Secure_Coding_C/Ex_Files_Secure_Coding_C/Exercise Files/Ch02/02_10/02_10-challenge1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	char *buffer;
+	int index;
+	char ch;
+	const char filename[] = "challenge.txt";
+	FILE *fp;
+
+	buffer = (char *)malloc( sizeof(char) * 32 );
+
+	printf("Type some text: ");
+	index = 0;
+	while( (ch=getchar()) )
+	{
+		*(buffer+index) = ch;
+		index++;
+		if( ch=='\n')
+			break;
+	}
+	/* cap the string */
+	*(buffer+index) = '\0';
+
+	fp = fopen(filename,"w");
+	fwrite(buffer,1,index,fp);
+	fclose(fp);
+	printf("%s created\n",filename);
+
+	return(0);
+}
+
